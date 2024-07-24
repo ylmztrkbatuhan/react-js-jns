c8424083068686ada1238ea84524afee95eb2387
oboy21 <oboy11b@gmail.com>
2024-07-24 16:59:24 +0300

    destructuring objects and arrays

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -142,3 +142,21 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+//Destructuring
+
+const book = getBook(2);
+book;
+// const title = book.title;
+// const author = book.author;
+
+const { title, author, pages, publicationDate, genres, hasMovieAdaptation } =
+  book;
+
+console.log(author, title, genres);
+
+// const primaryGenre = genres[0];
+// const secondaryGenre = genres[1];
+
+const [primaryGenre, secondaryGenre] = genres;
+
+console.log(primaryGenre, secondaryGenre);
